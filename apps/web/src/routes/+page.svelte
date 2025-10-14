38f195409029ae30f2f1ff877ec97c9694a9a42f
Brensen Villegas <brensen11@gmail.com>
2025-10-14 00:56:18 -0600

    ad map autism

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,3 +1,9 @@
 <script lang="ts">
+	import Map from '$lib/components/Map.svelte';
+
 	// main map page following figma design using map component and other features composed together
 </script>
+
+<Map>
+	<div class="absolute top-4 left-4 bg-red-600 rounded-md p-2"> TEST TEXT </div>
+</Map>
