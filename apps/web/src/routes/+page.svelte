c0799a7f29b29030bf4b9fc900b2d58ca605ea5a
Brensen Villegas <brensen11@gmail.com>
2025-10-15 16:41:18 -0600

    popup basic structure and styling added

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
-	import Map from '$lib/components/Map.svelte';
-
 	// main map page following figma design using map component and other features composed together
+	import Map from '$lib/components/Map.svelte';
+	import Popup from '$lib/components/Popup.svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
+
+	let open = $state(true);
+	const toggle = () => { open = !open };
 </script>
 
 <Map>
-	<div class="absolute top-4 left-4 bg-red-600 rounded-md p-2"> TEST TEXT </div>
+	<button onclick={toggle} class="absolute left-4 top-4 rounded-md bg-red-600 p-2 hover:cursor-pointer"> TEST TEXT </button>
+
+	<Popup
+		bind:open
+		class="absolute bottom-0 left-0 right-0 w-full"
+		title="Aaron's HQ"
+		courts_occupied={2}
+		groups_waiting={1}
+		estimated_wait_time_minutes={60}
+	></Popup>
 </Map>
